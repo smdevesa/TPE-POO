968a650a07497f6c609e41e18a545a1451362cf2
tizifuchi12 <tfuchinecco@itba.edu.ar>
2023-12-04 15:16:06 -0300

    efectos para elipsey mejora en drawableCircle

--- src/frontend/drawablemodel/DrawableCircle.java
+++ src/frontend/drawablemodel/DrawableCircle.java
@@ -17,6 +17,7 @@ import java.util.SortedSet;
 public class DrawableCircle extends Circle implements DrawableStyleableFigure {
 
     private double diameter = getRadius() * 2;
+    private final DrawableEllipse drawableEllipse = new DrawableEllipse(getCenterPoint(), diameter,diameter);
 
     public DrawableCircle(Point centerPoint, double radius) {
         super(centerPoint, radius);
@@ -24,37 +25,24 @@ public class DrawableCircle extends Circle implements DrawableStyleableFigure {
 
     @Override
     public void drawShadow(GraphicsContext gc, Color color) {
-        gc.setFill(Color.GREY);
-        drawFillWithOffset(gc, 10, 10);
+        drawableEllipse.drawShadow(gc, color);
     }
 
     @Override
     public void drawGradient(GraphicsContext gc, Color color) {
-        RadialGradient radialGradient = new RadialGradient(0, 0, 0.5, 0.5, 0.5, true,
-                CycleMethod.NO_CYCLE,
-                new Stop(0, color),
-                new Stop(1, color.invert()));
-        gc.setFill(radialGradient);
-        drawFillWithOffset(gc, 0, 0);
+        drawableEllipse.drawGradient(gc, color);
     }
 
     @Override
     public void drawBeveled(GraphicsContext gc, Color color) {
-        double arcX = getCenterPoint().getX() - getRadius();
-        double arcY = getCenterPoint().getY() - getRadius();
-        gc.setLineWidth(10);
-        gc.setStroke(Color.LIGHTGRAY);
-        gc.strokeArc(arcX, arcY, diameter, diameter, 45, 180, ArcType.OPEN);
-        gc.setStroke(Color.BLACK);
-        gc.strokeArc(arcX, arcY, diameter, diameter, 225, 180, ArcType.OPEN);
-        gc.setLineWidth(1);
+        drawableEllipse.drawBeveled(gc, color);
     }
 
     public void drawFillWithOffset(GraphicsContext gc, double offsetX, double offsetY) {
-        gc.fillOval(getCenterPoint().getX() - getRadius() + offsetX, getCenterPoint().getY() - getRadius() + offsetY, diameter, diameter);
+        drawableEllipse.drawFillWithOffset(gc, offsetX, offsetY);
     }
 
     public void drawBorder(GraphicsContext gc) {
-        gc.strokeOval(getCenterPoint().getX() - getRadius(), getCenterPoint().getY() - getRadius(), diameter, diameter);
+        drawableEllipse.drawBorder(gc);
     }
 }
--- src/frontend/drawablemodel/DrawableEllipse.java
+++ src/frontend/drawablemodel/DrawableEllipse.java
@@ -1,14 +1,13 @@
 package frontend.drawablemodel;
 
 import backend.model.Ellipse;
-import backend.model.Figure;
 import backend.model.Point;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
-
-import java.util.Map;
-import java.util.Set;
-import java.util.SortedSet;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.RadialGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.ArcType;
 
 public class DrawableEllipse extends Ellipse implements DrawableStyleableFigure {
 
@@ -16,35 +15,49 @@ public class DrawableEllipse extends Ellipse implements DrawableStyleableFigure 
         super(centerPoint, sMayorAxis, sMinorAxis);
     }
 
+/* CON ESTE METODO NO SE LE PUEDEN APLICAR EFECTOS A LAS ELIPSES (Y AHORA TAMPCOO A LOS CIRCLES). Tampoco se le dibuja el borde negro a elipse
     @Override
     public void draw(GraphicsContext gc, Map<Figure, Color> colorMap, Map<Figure, SortedSet<Effect>> effectMap, boolean selectionBorder) {
         gc.setFill(colorMap.get(this));
         gc.strokeOval(getCenterPoint().getX() - (getsMayorAxis() / 2), getCenterPoint().getY() - (getsMinorAxis() / 2), getsMayorAxis(), getsMinorAxis());
         gc.fillOval(getCenterPoint().getX() - (getsMayorAxis() / 2), getCenterPoint().getY() - (getsMinorAxis() / 2), getsMayorAxis(), getsMinorAxis());
-    }
+    }*/
 
     @Override
     public void drawBeveled(GraphicsContext gc, Color color) {
-
+        double arcX = getCenterPoint().getX() - getsMayorAxis()/2;
+        double arcY = getCenterPoint().getY() - getsMinorAxis()/2;
+        gc.setLineWidth(10);
+        gc.setStroke(Color.LIGHTGRAY);
+        gc.strokeArc(arcX, arcY, getsMayorAxis(), getsMinorAxis(), 45, 180, ArcType.OPEN);
+        gc.setStroke(Color.BLACK);
+        gc.strokeArc(arcX, arcY, getsMayorAxis(), getsMinorAxis(), 225, 180, ArcType.OPEN);
+        gc.setLineWidth(1);
     }
 
     @Override
     public void drawGradient(GraphicsContext gc, Color color) {
-
+        RadialGradient radialGradient = new RadialGradient(0, 0, 0.5, 0.5, 0.5, true,
+                CycleMethod.NO_CYCLE,
+                new Stop(0, color),
+                new Stop(1, color.invert()));
+        gc.setFill(radialGradient);
+        drawFillWithOffset(gc, 0, 0);
     }
 
     @Override
     public void drawShadow(GraphicsContext gc, Color color) {
-
+        gc.setFill(Color.GREY);
+        drawFillWithOffset(gc, 10, 10);
     }
 
     @Override
     public void drawFillWithOffset(GraphicsContext gc, double x, double y) {
-
+        gc.fillOval(getCenterPoint().getX() - (getsMayorAxis() / 2) + x, getCenterPoint().getY() - (getsMinorAxis() / 2) + y, getsMayorAxis(), getsMinorAxis());
     }
 
     @Override
     public void drawBorder(GraphicsContext gc) {
-
+        gc.strokeOval(getCenterPoint().getX() - (getsMayorAxis() / 2), getCenterPoint().getY() - (getsMinorAxis() / 2), getsMayorAxis(), getsMinorAxis());
     }
 }
